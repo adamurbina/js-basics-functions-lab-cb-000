fedffb7406489771ccf98b94c6986559f2cc3b64
Adam Urbina <adamurbina@gmail.com>
2019-01-12 11:56:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(){
+function distanceFromHqInBlocks(location){
+  let distance;
+
+  distance = Math.abs(location - 42)
   
+  return distance;
 }
 
 function distanceFromHqInFeet() {
-  
+
 }
 
 function distanceTravelledInFeet() {
-  
+
 }
 
 function calculatesFarePrice(){
